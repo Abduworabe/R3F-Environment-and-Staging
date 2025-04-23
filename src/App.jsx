d31ef175754b3cd4a2d03bffc7f4d4ec25a04a72
Abduworabe <abdilkerimjuhar@gmail.com>
2025-04-23 09:25:41 -0800

    Add r3f-perf for enhanced performance tracking in the 3D scene

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,52 @@
 import { Canvas, useFrame } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei'; // Import from drei for easier usage
 import { useRef } from 'react';
+import { Perf } from 'r3f-perf'; 
+
+// npm install r3f-perf
 function App() {
   const boxeRef = useRef();
   // This will run on every frame
-
-  // useFrame((state, delta) => {
-  //   //1, we need to know hoe much time has passed sone the last frame
-  //   //means delta time
-  //   boxeRef.current.rotation.y += delta;
-  //   // groupRef.current.rotation.y += delta;
-  // });
+  console.log(boxeRef)
+  useFrame((state, delta) => {
+    //   //1, we need to know hoe much time has passed sone the last frame
+    //   //means delta time
+    boxeRef.current.rotation.y += delta;
+    //   // groupRef.current.rotation.y += delta;
+  });
+  // Light
+  // <ambientLight>
+  //   <hemisphereLight
+  //   <directionalLight
+  //   <pointLight
+  //   <rectAreaLight
+  //   <spotLight
+  //1 Light helpers
 
   return (
     <>
-      <Canvas>
-        {/* If your scene is appearing black, there are a few common reasons for this when using three.js and @react-three/fiber. Here are some steps to troubleshoot and fix the */}
-        <ambientLight intensity={2} /> {/* Add ambient light */}
-        <pointLight position={[1, 2, 0]} intensity={20} />{' '}
-        {/* Add point light */}
-        <OrbitControls />
-        <mesh position-x={-1.5}>
-          <sphereGeometry />
-          <meshStandardMaterial color="green" />{' '}
-          {/* Set a color for the material */}
-        </mesh>
-        <mesh position-x={1.5} ref={boxeRef}>
-          <boxGeometry />
-          <meshStandardMaterial color="#ff0000" />
-        </mesh>
-        <mesh scale={10} rotation-x={-Math.PI * 0.25} position-y={-1.5}>
-          <planeGeometry />
-          <meshStandardMaterial color="#808000" />
-        </mesh>
-      </Canvas>
+
+      {/* If your scene is appearing black, there are a few common reasons for this when using three.js and @react-three/fiber. Here are some steps to troubleshoot and fix the */}
+      <ambientLight intensity={2} /> {/* Add ambient light */}
+      <pointLight position={[1, 2, 0]} intensity={20} />{' '}
+      {/* Add point light */}
+      <OrbitControls />
+      <Perf position='top-left'/>
+      {/* <Perf /> */}
+      <mesh position-x={-1.5}>
+        <sphereGeometry />
+        <meshStandardMaterial color="green" />{' '}
+        {/* Set a color for the material */}
+      </mesh>
+      <mesh position-x={1.5} ref={boxeRef}>
+        <boxGeometry />
+        <meshStandardMaterial color="#ff0000" />
+      </mesh>
+      <mesh scale={10} rotation-x={-Math.PI * 0.25} position-y={-1.5}>
+        <planeGeometry />
+        <meshStandardMaterial color="#808000" />
+      </mesh>
+
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
+import { Canvas } from '@react-three/fiber';
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+
+  <Canvas 
+  
+  camera={{
+    fov:45, 
+    near:0.1,
+    far:200,
+    position:[-4,3, 6]
+  }}>
+    <color args={['#b7ffc0']} attach="background"/>
+<App/>
+  </Canvas>
 );
